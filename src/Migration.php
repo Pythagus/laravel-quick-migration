1f5e81638dad241ddda2e69ce7e0dd23d157fdec
Damien MOLINA <dmolina@etud.insa-toulouse.fr>
2020-09-22 08:33:45 +0200

    Add class to get the migration table name

--- src/Migration.php
+++ src/Migration.php
@@ -2,7 +2,9 @@
 
 namespace Pythagus\LaravelQuickMigration;
 
+use Exception;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration as BaseMigration;
 
@@ -11,46 +13,76 @@ use Illuminate\Database\Migrations\Migration as BaseMigration;
  * @package Pythagus\LaravelQuickMigration
  *
  * @property string table
+ * @property Model  class
  *
  * @author: Damien MOLINA
  */
 abstract class Migration extends BaseMigration {
 
-    /**
-     * Name of the table
-     *
-     * @var string
-     */
-    protected $table;
+	/**
+	 * Name of the table
+	 *
+	 * @var string
+	 */
+	protected $table ;
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down() {
-        Schema::dropIfExists($this->table) ;
-    }
+	/**
+	 * Class name of the migration.
+	 *
+	 * @var Model
+	 */
+	protected $class ;
 
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up() {
-        if(! Schema::hasTable($this->table)) {
-            Schema::create($this->table, function(Blueprint $table) {
-                $this->structure($table);
-            }) ;
-        }
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 * @throws Exception
+	 */
+	public function down() {
+		Schema::dropIfExists($this->getTable()) ;
+	}
 
-    /**
-     * Structure of the table.
-     *
-     * @param Blueprint $table
-     * @return void
-     */
-    abstract public function structure(Blueprint $table);
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 * @throws Exception
+	 */
+	public function up() {
+		$table = $this->getTable() ;
+
+		if(! Schema::hasTable($table)) {
+			Schema::create($table, function(Blueprint $t) {
+				$this->structure($t);
+			}) ;
+		}
+	}
+
+	/**
+	 * Get the table name.
+	 *
+	 * @return string
+	 * @throws Exception
+	 */
+	protected function getTable() {
+		if(! empty($class = $this->class)) {
+			return app($class)->getTable() ;
+		}
+
+		if(! empty($table = $this->table)) {
+			return $table ;
+		}
+
+		throw new Exception("Undefined class in ".static::class." migration.") ;
+	}
+
+	/**
+	 * Structure of the table.
+	 *
+	 * @param Blueprint $table
+	 * @return void
+	 */
+	abstract public function structure(Blueprint $table);
 
 }
